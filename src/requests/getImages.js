43342a55f7d13d09ec6fa14c37641a401148105b
Andy Jackson <andypauljackson@gmail.com>
2022-10-13 09:09:48 +0100

    adding getImage query

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+const getImages = (query) => {
+    if (!query) {
+        return Promise.resolve([]);
+    } else {
+        return axios
+        .get(`https://images-api.nasa.gov/search?q=moon`)
+    }
+}
+
+export default getImages
