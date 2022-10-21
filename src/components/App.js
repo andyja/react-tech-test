a9a88825831c2165397c492220c3722b17635ece
Andy Jackson <andypauljackson@gmail.com>
2022-10-21 13:02:47 +0100

    refactor: more efficient search

--- src/components/App.js
+++ src/components/App.js
@@ -20,5 +20,3 @@ function App() {
 }
 
 export default App;
-
-//searchResults.map((e) => console.log(e))
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,31 @@
-import React, { useState } from "react";
-// import React, { useRef } from "react";
+import React, { useRef } from "react";
 import "../styles/search.css";
 import "../requests/getImages";
 import getImages from "../requests/getImages";
 
 
 const Search = ({ setSearchResults }) => {
-    const [value, setValue] = useState();
+    const searchBox = useRef(null);
 
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    setSearchResults(await getImages(value));
-};
 
-        // function SearchForm(props) {
-        // const { onSubmit } = props;
-  
-        // const inputElement = useRef(null);
-
-        // <button
-        // type="submit"
-        // onClick={() => {
-        //   onSubmit(inputElement.current.value);
-        // }}
-        // ></button>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSearchResults(await getImages(searchBox.current.value));
+    };
 
     return (
         <div className="search">
-        <form className="search-form" onSubmit={handleSubmit}>
-        <input className="search-input" 
-        type="text" placeholder="search here for nasa images..."
-        onChange={(e) => setValue(e.target.value)}
-        />
-        <button className="search-btn" type="submit">
-            Go!
-        </button>
-        </form>
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input
+                    className="search-input" 
+                    type="text"
+                    placeholder="search here for nasa images..."
+                    ref={searchBox}
+                />
+                <button className="search-btn" type="submit">
+                    Go!
+                </button>
+            </form>
         </div>
     );
 };
